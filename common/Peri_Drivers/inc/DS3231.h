a38a66d6371d8c1ca4377a2d71a3aff1d5d4bedd
taktopanie <maciejzwolan@gmail.com>
2024-01-03 16:20:17 +0100

    Library code update with read/write option

--- common/Peri_Drivers/inc/DS3231.h
+++ common/Peri_Drivers/inc/DS3231.h
@@ -10,11 +10,42 @@
 
 #include "stm32f3xx_hal.h"
 
+
+#define DS3231_address 			0b11010001
+
 /*
  * @ALARM_NUMBERS
  */
-#define ALARM_1			1
-#define ALARM_2			2
+#define ALARM_1					1
+#define ALARM_2					2
+
+
+
+/*
+ * DS3231 registers
+ */
+
+#define DS3231_seconds_addr		0x0
+#define DS3231_minutes_addr		0x1
+#define DS3231_hours_addr		0x2
+#define DS3231_days_addr		0x3
+#define DS3231_date_addr		0x4
+#define DS3231_month_addr		0x5
+#define DS3231_year_addr		0x6
+#define DS3231_alarm1m1_addr	0x7
+#define DS3231_alarm1m2_addr	0x8
+#define DS3231_alarm1m3_addr	0x9
+#define DS3231_alarm1m4_addr	0xA
+#define DS3231_alarm2m2_addr	0xB
+#define DS3231_alarm2m3_addr	0xC
+#define DS3231_alarm2m4_addr	0xD
+
+#define DS3231_control_addr		0xE
+#define DS3231_status_addr		0xF
+#define DS3231_aging_addr		0x10
+#define DS3231_msbTemp_addr		0x11
+#define DS3231_lsbTemp_addr		0x12
+
 
 typedef struct{
 	uint8_t time_hr;
@@ -54,5 +85,8 @@ uint8_t DS3231_get_alarm(I2C_HandleTypeDef * I2C_PERI, uint8_t Alarm_No);
 
 void DS3231_set_alarm(I2C_HandleTypeDef * I2C_PERI , uint8_t Alarm_No);
 void DS3231_clear_alarm(I2C_HandleTypeDef * I2C_PERI , uint8_t Alarm_No);
+
+void DS3231_data_write(I2C_HandleTypeDef * I2C_PERI, uint8_t peri_address, uint8_t mem_address, uint8_t* data_to_send, uint8_t data_length);
+void DS3231_data_read(I2C_HandleTypeDef * I2C_PERI, uint8_t peri_address, uint8_t mem_address, uint8_t* read_buffor, uint8_t data_length);
 
 #endif /* PERI_DRIVERS_INC_DS3231_H_ */
--- common/Peri_Drivers/src/DS3231.c
+++ common/Peri_Drivers/src/DS3231.c
@@ -19,136 +19,139 @@ uint8_t _dec_2_hex(uint8_t dec)
 DS3231_Time_t DS3231_get_time(I2C_HandleTypeDef * I2C_PERI)
 {
 	DS3231_Time_t tmp_data = {0, 0, 0};
-	uint8_t data_to_send = 0;
-	uint8_t BUFFER [200];
+	uint8_t BUFFOR [200];
 
 
 	/// GET SECONDS
-	data_to_send = 0x00;
+	DS3231_data_read(I2C_PERI, DS3231_address, DS3231_seconds_addr, BUFFOR, 1);
 
-	HAL_I2C_Master_Transmit(I2C_PERI, 0b11010000, &data_to_send , 1, 1000);
-	HAL_I2C_Master_Receive(I2C_PERI, 0b11010000, BUFFER, 1, 100);
-
-	tmp_data.time_sec = (BUFFER[0] & 0xF) + 10*((BUFFER[0] >> 4) & 0x7);
+	tmp_data.time_sec = (BUFFOR[0] & 0xF) + 10*((BUFFOR[0] >> 4) & 0x7);
 
 	/// GET MINUTES
-	data_to_send = 0x01;
+	DS3231_data_read(I2C_PERI, DS3231_address, DS3231_minutes_addr, BUFFOR, 1);
 
-	HAL_I2C_Master_Transmit(I2C_PERI, 0b11010000, &data_to_send , 1, 1000);
-	HAL_I2C_Master_Receive(I2C_PERI, 0b11010000, BUFFER, 1, 100);
-
-	tmp_data.time_min = (BUFFER[0] & 0xF) + 10*((BUFFER[0] >> 4) & 0x7);
+	tmp_data.time_min = (BUFFOR[0] & 0xF) + 10*((BUFFOR[0] >> 4) & 0x7);
 
 	/// GET HOURS
-	data_to_send = 0x02;
+	DS3231_data_read(I2C_PERI, DS3231_address, DS3231_hours_addr, BUFFOR, 1);
 
-	HAL_I2C_Master_Transmit(I2C_PERI, 0b11010000, &data_to_send , 1, 1000);
-	HAL_I2C_Master_Receive(I2C_PERI, 0b11010000, BUFFER, 1, 100);
-
-	tmp_data.time_hr = (BUFFER[0] & 0xF) + 10*((BUFFER[0] >> 4) & 0x3);
+	tmp_data.time_hr = (BUFFOR[0] & 0xF) + 10*((BUFFOR[0] >> 4) & 0x3);
 
 	return tmp_data;
 }
 
 uint8_t DS3231_set_time(I2C_HandleTypeDef * I2C_PERI, DS3231_Time_t TIME){
-		uint8_t data_to_send [2];
+	uint8_t data_to_send;
 
-		//SET SECONDS
-		//address
-		data_to_send[0] = 0x0;
-		//data
-		data_to_send[1] = _dec_2_hex(TIME.time_sec) & 0x7F;
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+	//SET SECONDS
+	data_to_send = _dec_2_hex(TIME.time_sec) & 0x7F;
+	DS3231_data_write(I2C_PERI, DS3231_address, DS3231_seconds_addr, &data_to_send , 1);
 
-		//SET MINUTES
-		//address
-		data_to_send[0] = 0x1;
-		//data
-		data_to_send[1] = _dec_2_hex(TIME.time_min) & 0x7F;
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+	//SET MINUTES
+	data_to_send = _dec_2_hex(TIME.time_min) & 0x7F;
+	DS3231_data_write(I2C_PERI, DS3231_address, DS3231_minutes_addr, &data_to_send , 1);
 
-		//SET HOURS
-		//address
-		data_to_send[0] = 0x2;
-		//data
-		data_to_send[1] = _dec_2_hex(TIME.time_hr) & 0x3F;
-
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
-
+	//SET HOURS
+	data_to_send = _dec_2_hex(TIME.time_hr) & 0x3F;
+	DS3231_data_write(I2C_PERI, DS3231_address, DS3231_hours_addr, &data_to_send , 1);
 
 	return 0;
 }
 
 void DS3231_set_alarm(I2C_HandleTypeDef * I2C_PERI , uint8_t Alarm_No){
 
-	uint8_t data_to_send [2];
+	uint8_t data_to_send;
 
 	//TODO rest options == NOW ONLY ONCE PER SECOND
 	if(Alarm_No == ALARM_1)
 	{
+		data_to_send = 0x80;
 
-		//address A1M1
-		data_to_send[0] = 0x7;
-		//data
-		data_to_send[1] = 0x80;
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+		//address A1M1 = 0x7
+		DS3231_data_write(I2C_PERI, DS3231_address, DS3231_alarm1m1_addr, &data_to_send , 1);
 
-		//address A1M2
-		data_to_send[0] = 0x8;
-		//data
-		data_to_send[1] = 0x80;
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+		data_to_send = 0x80;
+		//address A1M2 = 0x8
+		DS3231_data_write(I2C_PERI, DS3231_address, DS3231_alarm1m2_addr, &data_to_send , 1);
 
-		//address A1M3
-		data_to_send[0] = 0x9;
-		//data
-		data_to_send[1] = 0x80;
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+		data_to_send = 0x80;
+		//address A1M3 = 0x9
+		DS3231_data_write(I2C_PERI, DS3231_address, DS3231_alarm1m3_addr, &data_to_send , 1);
 
-		//address A1M4
-		data_to_send[0] = 0xA;
-		//data
-		data_to_send[1] = 0x80;
-		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+		data_to_send = 0x80;
+		//address A1M4 = 0xA
+		DS3231_data_write(I2C_PERI, DS3231_address, DS3231_alarm1m4_addr, &data_to_send , 1);
 
 
 	}else if(Alarm_No == ALARM_2)
 	{
-
+		//TODO:
 	}
 }
 
 uint8_t DS3231_get_alarm(I2C_HandleTypeDef * I2C_PERI , uint8_t Alarm_No)
 {
-	uint8_t BUFFER;
+	uint8_t BUFFOR;
 
 	//Status register address 0xF
-	uint8_t data_to_send = 0xF;
+	DS3231_data_read(I2C_PERI, DS3231_address, DS3231_status_addr, &BUFFOR, 1);
 
-	HAL_I2C_Master_Transmit(I2C_PERI, 0b11010000, &data_to_send , 1, 1000);
-	HAL_I2C_Master_Receive(I2C_PERI, 0b11010000, &BUFFER, 1, 100);
-
-	return(BUFFER & Alarm_No);
+	return(BUFFOR & Alarm_No);
 
 }
 
 void DS3231_clear_alarm(I2C_HandleTypeDef * I2C_PERI , uint8_t Alarm_No)
 {
-	uint8_t data_to_send [2];
-	//address of the status register
-	data_to_send[0] = 0xF;
-
 	//Clear all the flags
-	data_to_send[1] = 0x00;
+	uint8_t data_to_send = 0x0;
 
-	HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, &data_to_send[0] , 2, 1000);
+	DS3231_data_write(I2C_PERI, DS3231_address, 0xF, &data_to_send , 1);
+
+	//HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, &data_to_send[0] , 2, 1000);
 }
 
-/*TODO FUNCTION TO WRITE DATA IN DS3231
+/*
+ * DS3231_data_write - send the data to DS3231
  *
+ * @params:
+ * 	I2C_PERI				- BUS address pointer (I2C)
+ * 	peri_address			- address of peripheral (device)
+ * 	mem_address				- memory address
+ * 	data_to_send			- pointer to data which will be send
+ * 	data_length				- number of data which will be send
  *
- *		data_to_send[0] = address;
- *		data_to_send[1] = data;
- *		HAL_I2C_Master_Transmit(I2C_PERI, 0b11010001, data_to_send , 2, 1000);
+ */
+void DS3231_data_write(I2C_HandleTypeDef * I2C_PERI, uint8_t peri_address, uint8_t mem_address, uint8_t* data_to_send, uint8_t data_length)
+{
+	uint8_t data_buff[100];
+
+	//first send memory address
+	data_buff[0] = mem_address;
+
+	//fill the buffor with data
+	for(int i = 0; i < data_length; i++)
+	{
+		data_buff[i+1] = data_to_send[i];
+	}
+
+	//transmit the buffor
+	HAL_I2C_Master_Transmit(I2C_PERI, peri_address, data_buff , (data_length + 1), 1000);
+}
+
+/*
+ * DS3231_data_read - read the data from DS3231
  *
-*/
+ * @params:
+ * 	I2C_PERI				- BUS address pointer (I2C)
+ * 	peri_address			- address of peripheral (device)
+ * 	mem_address				- memory address
+ * 	read_buffor				- buffor which will be refiled with the read data
+ * 	data_length				- number of data which will be received
+ *
+ */
+void DS3231_data_read(I2C_HandleTypeDef * I2C_PERI, uint8_t peri_address, uint8_t mem_address, uint8_t* read_buffor, uint8_t data_length)
+{
+	HAL_I2C_Master_Transmit(I2C_PERI, peri_address, &mem_address , 1, 1000);
+	HAL_I2C_Master_Receive(I2C_PERI, peri_address, read_buffor, data_length, 100);
+
+}
