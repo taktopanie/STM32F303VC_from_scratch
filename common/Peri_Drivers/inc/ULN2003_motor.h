ed8c2d0f188bafab28e8858f21d93c210ca9f462
taktopanie <maciejzwolan@gmail.com>
2024-02-13 21:13:52 +0100

    Motor idle state and PIN definitions added

--- common/Peri_Drivers/inc/ULN2003_motor.h
+++ common/Peri_Drivers/inc/ULN2003_motor.h
@@ -12,6 +12,19 @@
 #include "stm32f3xx_hal_gpio.h"
 #include "stm32f3xx.h"
 
+/*
+ * MOTOR PIN DEFINITION
+ */
+#define MOTOR_PIN_0			GPIO_PIN_0
+#define MOTOR_PORT_0		GPIOC
+#define MOTOR_PIN_1			GPIO_PIN_1
+#define MOTOR_PORT_1		GPIOC
+#define MOTOR_PIN_2			GPIO_PIN_2
+#define MOTOR_PORT_2		GPIOC
+#define MOTOR_PIN_3			GPIO_PIN_3
+#define MOTOR_PORT_3		GPIOC
+
+
 
 void MOTOR_set_position(uint8_t pos);
 void MOTOR_left (void);
